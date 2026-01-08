ae5e6f632ec60cb040020b466f3b42a01aa6b2e8
hafilpk <pkmshafil@gmail.com>
2026-01-08 23:44:11 +0530

    featured post

--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
 
 from django.shortcuts import render
-from blog.models import Category
+from blog.models import Category, Blog
 
 def home(request):
+    featured = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
     categories = Category.objects.all()
     context = {
         'categories':categories,
+        'featured': featured,
     }
     return render(request, "home.html", context)
