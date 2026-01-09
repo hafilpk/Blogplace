05ab266df00b4745ebd44d1a8ffba17436b889ec
hafilpk <pkmshafil@gmail.com>
2026-01-09 22:15:27 +0530

    Blog posts from database to UI

--- main/views.py
+++ main/views.py
@@ -5,8 +5,10 @@ from blog.models import Category, Blog
 def home(request):
     featured = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
     categories = Category.objects.all()
+    post = Blog.objects.filter(is_featured=False, status='Published')
     context = {
         'categories':categories,
         'featured': featured,
+        'post': post,
     }
     return render(request, "home.html", context)
