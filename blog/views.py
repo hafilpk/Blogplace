db78c5b8d3e79687b37a7e18a89e8097d6180313
hafilpk <pkmshafil@gmail.com>
2026-01-11 17:09:43 +0530

    Category page created and made functional

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Blog, Category
 
-# Create your views here.
+
+def category_page(request, category_id):
+    posts = Blog.objects.filter(status='Published', category=category_id)
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        return redirect('home')
+
+    context = {
+        'posts': posts,
+        'category': category,
+    }
+    return render(request, 'category_page.html', context)
