db05005b7cf8c1af71d7542f6f1e0dc95a4eae81
hafilpk <pkmshafil@gmail.com>
2026-02-18 23:10:32 +0530

    Updated dashboard with login_required decorator and dynamic count

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from blog.models import Blog, Category
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='login')
 def dashboard(request):
-    return render(request, 'dashboards/dashboard.html')
+    category_count = Category.objects.all().count()
+    blogs_count = Blog.objects.all().count()
+
+    context = {
+        'category_count': category_count,
+        'blogs_count': blogs_count,
+    }
+    return render(request, 'dashboards/dashboard.html', context)
